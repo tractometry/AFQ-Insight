ad4c020125bcce4fdaf84f62f735a69746abe031
sam chou <sam@thechous.com>
2024-11-06 15:26:03 -0800

    change line length

--- afqinsight/nn/tests/test_compare_py_tf.py
+++ afqinsight/nn/tests/test_compare_py_tf.py
@@ -21,42 +21,36 @@ from afqinsight.nn.tf_models import (
 from afqinsight.nn.utils import compare_models
 
 
-# test for mlp4
 def test_mlp4():
     pytorch_mlp4 = mlp4_pt(input_shape=784, n_classes=10)
     tensorflow_mlp4 = mlp4(input_shape=(784,), n_classes=10, verbose=True)
     assert compare_models(pytorch_mlp4, tensorflow_mlp4)
 
 
-# # test for cnn_lenet
 def test_cnn_lenet():
     pytorch_cnn_lenet = cnn_lenet_pt(input_shape=(784, 1), n_classes=10)
     tensorflow_cnn_lenet = cnn_lenet(input_shape=(784, 1), n_classes=10, verbose=True)
     assert compare_models(pytorch_cnn_lenet, tensorflow_cnn_lenet)
 
 
-# # # test for cnn_vgg
 def test_cnn_vgg():
     pytorch_cnn_vgg = cnn_vgg_pt(input_shape=(1, 784, 1), n_classes=10)
     tensorflow_cnn_vgg = cnn_vgg(input_shape=(1, 784, 1), n_classes=10, verbose=True)
     assert compare_models(pytorch_cnn_vgg, tensorflow_cnn_vgg)
 
 
-# test for LSTM1V0
 def test_lstm1v0():
     pytorch_lstm1v0 = lstm1v0_pt(input_shape=(784, 1), n_classes=10)
     tensorflow_lstm1v0 = lstm1v0(input_shape=(784, 1), n_classes=10, verbose=True)
     assert compare_models(pytorch_lstm1v0, tensorflow_lstm1v0)
 
 
-# test for LSTM1
 def test_lstm1():
     pytorch_lstm1 = lstm1_pt(input_shape=(784, 1), n_classes=10)
     tensorflow_lstm1 = lstm1(input_shape=(784, 1), n_classes=10, verbose=True)
     assert compare_models(pytorch_lstm1, tensorflow_lstm1)
 
 
-# test for LSTM2
 def test_lstm2():
     pytorch_lstm2 = lstm2_pt(input_shape=(784, 1), n_classes=10)
     tensorflow_lstm2 = lstm2(input_shape=(784, 1), n_classes=10, verbose=True)
@@ -73,3 +67,17 @@ def test_blstm2():
     pytorch_blstm2 = blstm2_pt(input_shape=(784, 1), n_classes=10)
     tensorflow_blstm2 = blstm2(input_shape=(784, 1), n_classes=10, verbose=True)
     assert compare_models(pytorch_blstm2, tensorflow_blstm2)
+
+
+# def test_lstm_fcn():
+#     pytorch_lstm_fcn = lstm_fcn_pt(input_shape=(784, 1), n_classes=10)
+#     tensorflow_lstm_fcn = lstm_fcn(input_shape=(784, 1), n_classes=10, verbose=True)
+#     summary(pytorch_lstm_fcn, input_size=(1, 784, 1))
+#     assert compare_models(pytorch_lstm_fcn, tensorflow_lstm_fcn)
+
+# def test_cnn_resnet():
+#     pytorch_cnn_resnet = cnn_resnet_pt(input_shape=(784, 1), n_classes=10)
+#     tensorflow_cnn_resnet = cnn_resnet(input_shape=(784, 1),
+#                                        n_classes=10, verbose=True)
+#     summary(pytorch_cnn_resnet, input_size=(1, 784, 1))
+#     assert compare_models(pytorch_cnn_resnet, tensorflow_cnn_resnet)
