3f759e5b18e17ab8b8e2e9c61b427ef05cec0ad6
sam chou <sam@thechous.com>
2024-12-02 16:44:31 -0800

    removing unused lines

--- afqinsight/nn/pt_models.py
+++ afqinsight/nn/pt_models.py
@@ -135,9 +135,6 @@ class CNN_VGG(nn.Module):
         super(CNN_VGG, self).__init__()
 
         n_conv_layers = int(round(math.log(input_shape[0], 2)) - 3)
-        # if verbose:
-        #     print(f"Pooling layers: {n_conv_layers}")
-
         conv_layers = []
         in_channels = input_shape[0]
 
