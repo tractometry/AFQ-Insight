cb0ce77dec844fbede39d6120e8a1952e06d19bf
Ariel Rokem <arokem@gmail.com>
2024-12-06 15:59:48 -0800

    Reinstate this state.

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 import os.path as op
+import string
 
 from setuptools import setup
+from setuptools_scm import get_version
 
 
 def local_version(version):
     """Patch in a version that can be uploaded to test PyPI."""
-    return ""
+    scm_version = get_version()
+    if "dev" in scm_version:
+        gh_in_int = []
+        for char in version.node:
+            if char.isdigit():
+                gh_in_int.append(str(char))
+            else:
+                gh_in_int.append(str(string.ascii_letters.find(char)))
+        return "".join(gh_in_int)
+    else:
+        return ""
 
 
 opts = {
